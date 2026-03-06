2465eaa17aef5a693359323c9a6056a24d3e4c2e
Roman Vasilyev <vasilyev-r@mail.ru>
2026-03-06 23:28:54 +0300

    Test theme

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
 get_header();
 ?>
 
-<?php get_template_part('template-parts/hero/hero'); ?>
+<?php echo 'Test'; //get_template_part('template-parts/hero/hero'); ?>
 
-<?php get_template_part('template-parts/advantages/advantages'); ?>
+<?php //get_template_part('template-parts/advantages/advantages'); ?>
 
-<?php get_template_part('template-parts/section-catalog-categories/section-catalog-categories'); ?>
+<?php //get_template_part('template-parts/section-catalog-categories/section-catalog-categories'); ?>
 
-<?php get_template_part('template-parts/partners/partners'); ?>
+<?php //get_template_part('template-parts/partners/partners'); ?>
 
-<?php get_template_part('template-parts/testimonials/testimonials'); ?>
+<?php //get_template_part('template-parts/testimonials/testimonials'); ?>
 
-<?php get_template_part('template-parts/order-gradient/order-gradient'); ?>
+<?php //get_template_part('template-parts/order-gradient/order-gradient'); ?>
 
-<?php get_template_part('template-parts/license/license'); ?>
+<?php //get_template_part('template-parts/license/license'); ?>
 
-<?php get_footer(null, ['bg_corporate' => true]); ?>
+<?php //get_footer(null, ['bg_corporate' => true]); ?>
