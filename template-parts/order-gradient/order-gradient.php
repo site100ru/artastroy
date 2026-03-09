b76c7876cebc68ef578d5e3a4d275d043c9ef283
Slava Sidorov <sidorov-vv3@mail.ru>
2026-03-09 14:47:15 +0500

    обновил стили по новому дизайну

--- template-parts/order-gradient/order-gradient.php
+++ template-parts/order-gradient/order-gradient.php
@@ -25,29 +25,30 @@ $whatsapp = mytheme_get_whatsapp();
 ?>
 
 <!-- ========== ОСТАЛИСЬ ВОПРОСЫ ========== -->
-<section class="section order-right-gradient-section bg-corporate">
+<section class="section order-left-gradient-section bg-corporate-alt">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-6"></div>
-            <div class="col-md-6 col-xl-5">
+            <div class="col-xl-1"></div>
+
+            <div class="col-md-6 col-xl-5 order-left-gradient-content">
 
                 <h2 class="section-title order-section-title"><?php echo esc_html($title); ?></h2>
                 <p class="section-description order-section-desc mb-3"><?php echo esc_html($description); ?></p>
 
-                <form method="post" action="<?php echo esc_attr($action); ?>">
+                <form method="post" action="<?php echo esc_attr($action); ?>"> 
                     <div class="row">
                         <div class="col-6 col-md-5">
                             <label for="order-gradient-name" class="order-form-label">Ваше имя</label>
-                            <input type="text" name="name" class="order-form-input mb-3 mb-md-0" id="order-gradient-name">
+                            <input type="text" name="name" class="order-form-input mb-3 mb-md-0 order-form-input-dark" id="order-gradient-name">
                         </div>
                         <div class="col-6 col-md-5">
                             <label for="order-gradient-phone" class="order-form-label">Ваш телефон*</label>
-                            <input type="text" name="phone" class="order-form-input mb-3 mb-md-0 telMask" id="order-gradient-phone" inputmode="text" required>
+                            <input type="text" name="phone" class="order-form-input mb-3 mb-md-0 order-form-input-dark telMask" id="order-gradient-phone" inputmode="text" required>
                         </div>
                     </div>
                     <div class="row mt-md-3">
                         <div class="col-6 col-md-7 col-lg-5">
-                            <button type="submit" class="btn btn-corporate-color-1 mt-md-3 w-100">
+                            <button type="submit" class="btn btn-corporate-color-1 btn-corporate-color-2 mt-md-3 w-100">
                                 <?php echo esc_html($button_text); ?>
                             </button>
                         </div>
@@ -60,15 +61,15 @@ $whatsapp = mytheme_get_whatsapp();
                             <ul class="nav">
                                 <?php if ($telegram) : ?>
                                     <li class="nav-item">
-                                        <a class="nav-link ico-button p-0 pe-3 pe-md-4" href="<?php echo esc_url($telegram); ?>">
-                                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/ico/telegram-circle-ico.svg" alt="Telegram">
+                                        <a class="nav-link ico-button ico-button-alt p-0 pe-3 pe-md-4" href="<?php echo esc_url($telegram); ?>">
+                                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/ico/telegram-icon-dark.svg" alt="Telegram">
                                         </a>
                                     </li>
                                 <?php endif; ?>
                                 <?php if ($whatsapp) : ?>
                                     <li class="nav-item">
-                                        <a class="nav-link ico-button p-0 pe-3 pe-md-4" href="<?php echo esc_url($whatsapp); ?>">
-                                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/ico/whatsapp-circle-ico.svg" alt="WhatsApp">
+                                        <a class="nav-link ico-button ico-button-alt p-0 pe-3 pe-md-0" href="<?php echo esc_url($whatsapp); ?>">
+                                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/ico/wa-icon-dark.svg" alt="WhatsApp">
                                         </a>
                                     </li>
                                 <?php endif; ?>
@@ -78,7 +79,8 @@ $whatsapp = mytheme_get_whatsapp();
                 <?php endif; ?>
 
             </div>
-            <div class="col-xl-1"></div>
+
+            <div class="col-md-6"></div>
         </div>
     </div>
     <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/order-sections-mobile.jpg" class="w-100 d-md-none" alt="">
