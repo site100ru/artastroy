0a9b176c3f6922f4f93deea4abc60f34c4430d80
/usr/bin/vim <support@beget.ru>
2026-04-24 10:19:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ get_header();
 
 <?php get_template_part('template-parts/partners/partners'); ?>
 
-<?php get_template_part('template-parts/testimonials/testimonials'); ?>
+<!--?php get_template_part('template-parts/testimonials/testimonials'); ?-->
 
 <?php get_template_part('template-parts/order-gradient/order-gradient'); ?>
 
-<?php get_template_part('template-parts/license/license'); ?>
+<!--?php get_template_part('template-parts/license/license'); ?-->
 
 <?php get_footer(null, ['bg_corporate' => true]); ?>
